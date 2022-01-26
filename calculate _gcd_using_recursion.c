8eb914a75c5b4bb56f5e4ce904ff9db9e3c0c0a9
GourinathRoy <98100565+GourinathRoy@users.noreply.github.com>
2022-01-26 21:12:22 +0530

    Create calculate _gcd_using_recursion.c

---
+++ calculate _gcd_using_recursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int gcd (int,int);
+int main()
+{
+	int result;
+	result=gcd(10,5);
+	printf("the result =%d",result);
+	return 0;
+}
+	int gcd(int a,int b)
+	{
+		while(a!=b)
+		{
+			if(a>b)
+			{
+				return gcd(a-b,b);
+			}
+				else
+				{
+					return gcd(a,b-a);
+				}
+		}
+		return a;
+	}
