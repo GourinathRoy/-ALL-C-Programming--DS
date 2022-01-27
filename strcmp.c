2ff0a0d0bfc1c6b76ff629d869469ba63066010b
GourinathRoy <98100565+GourinathRoy@users.noreply.github.com>
2022-01-27 11:04:31 +0530

    Create strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int c;
+char a[]="gourinath";
+char b[]="gourinath";
+c=strcmp(b,a);
+if(c==0)
+{
+	printf("values are same");
+}
+else
+{
+	printf("values are not same");
+}
+
+
+
+}
